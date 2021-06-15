684c25272f6a69bfced6daaa95455a39eef9de67
aniekutmfongodwin <adriano8263@gmail.com>
2021-06-15 15:49:44 +0100

    classes

---
+++ classes.js
@@ -0,0 +1,48 @@
+
+
+
+class PowerBank{
+    constructor(length,name,height,width,chargingPort,powerLevel){
+            this.length = length
+            this.height = height
+            this.name = name
+            this.width = width
+            this.chargingPort = chargingPort
+            this.powerLevel = powerLevel
+
+//                 console.log("this is called")
+
+    }
+
+
+    
+    powerIn(){
+
+            console.log("power bank charging .level",this.powerLevel)
+
+    }
+
+
+    powerOut(){
+            this.yourName()
+         console.log("phone has been pluged")
+    }
+
+    yourName(){
+            console.log("my name is ",this.name)
+    }
+
+
+
+}
+
+
+
+class NewPowerBank extends PowerBank{
+    
+    powerIn(){
+
+            console.log("power bank charging .level",this.powerLevel)
+
+    }
+}
